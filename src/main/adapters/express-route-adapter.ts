48734df35fce70616eb239eab790f1b6ef1bbc4b
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-14 21:18:57 -0300

    feat: incluida rota de adicionar estilo

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,19 @@
+import { IController } from "@/presentation/interfaces";
+
+import { Request, Response } from "express";
+
+export const adaptRoute = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      body: req.body
+    };
+    const httpResponse = await controller.handle(request);
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      });
+    }
+  };
+};
