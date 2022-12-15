43f84dd1a8e4fef8e010562f2f35b9be89b8924c
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-14 21:18:26 -0300

    feat: configurado express

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,19 @@
 import "./config/module-alias";
 
+import "dotenv/config";
 import "reflect-metadata";
 
-console.log("init");
+import { app } from "@/main/config/app";
+import { PostgresDataSource } from "@/infra/database/pg-datasource";
+
+const port = process.env.APP_PORT ?? "8000";
+
+PostgresDataSource.initialize()
+  .then(() => {
+    app.listen(port, () => console.log(`server running at http://localhost:${port}`));
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+// init
