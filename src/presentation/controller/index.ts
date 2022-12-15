bbbae03e0f8a3d0378f581e2ea0a7eb6a5ac7277
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-15 18:11:28 -0300

    feat: adicionado DeleteBeerStyleController

--- src/presentation/controller/index.ts
+++ src/presentation/controller/index.ts
@@ -2,3 +2,4 @@ export * from "./add-beer-style-controller";
 export * from "./getall-beer-style-controller";
 export * from "./get-beer-style-controller";
 export * from "./update-beer-style-controller";
+export * from "./delete-beer-style-controller";
