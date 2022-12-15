0cd9e77059d7c61a77e4002b51a180bc65cb5cd1
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-15 18:18:06 -0300

    feat: adicionado rota para delecao de estilo

--- src/main/factories/index.ts
+++ src/main/factories/index.ts
@@ -2,3 +2,4 @@ export * from "./add-beer-style-factory";
 export * from "./getall-beer-style-factory";
 export * from "./update-beer-style-factory";
 export * from "./get-beer-style-factory";
+export * from "./delete-beer-style-factory";
