e1d8f8358d2a16581df83da1564e970e04e075a7
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-17 00:40:00 -0300

    feat: adicionado schemas para validacao dos dados do AddBeerStyle e UpdateBeerStyle

---
+++ src/infra/validator/joi-validator/index.ts
@@ -0,0 +1,3 @@
+export * from "@/infra/validator/joi-validator/add-beer-style-schema";
+export * from "@/infra/validator/joi-validator/joi-schema-validator";
+export * from "@/infra/validator/joi-validator/update-beer-style-schema";
