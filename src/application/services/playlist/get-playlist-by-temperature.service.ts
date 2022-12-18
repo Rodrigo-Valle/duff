93c3f8340b3f3300f6006134fbee23bbaf51efac
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-18 19:16:56 -0300

    refactor: remocao do pg-mem e colocado postgres rodando no docker no lugar

---
+++ src/application/services/playlist/get-playlist-by-temperature.service.ts
@@ -0,0 +1,12 @@
+// import { BeerStyleRepository, SpotifyApi } from "@/application/interfaces";
+// import { Playlist } from "@/domain/models";
+// import { GetPlaylistByTemperature } from "@/domain/usecases/playlist";
+
+// export class GetPlaylistByTemperatureService implements: GetPlaylistByTemperature {
+//   constructor(private readonly repository: BeerStyleRepository) { }
+
+//   async get(temperature: number): Promise<Playlist> {
+//     const beerStyle = await this.repository.findByTemperatureAverage(temperature);
+//   }
+
+// }
--- tests/infra/repositories/pg-beer-style-repository.test.ts
+++ tests/infra/repositories/pg-beer-style-repository.test.ts
@@ -1,27 +1,21 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { BeerStyleDBEntity } from "@/infra/entities";
 import { PostgresBeerStyleRepository } from "@/infra/repositories";
-import { makeBeerStyleDTO, makemockedDbConnection } from "@/tests/infra/mocks";
+import { makeBeerStyleDTO } from "@/tests/infra/mocks";
+import { PostgresDataSource } from "@/infra/database/pg-datasource";
 
-import { IBackup, IMemoryDb } from "pg-mem";
 import { DataSource, Repository } from "typeorm";
 
 describe("PostgresBeerStyleRepository tests", () => {
   let sut: PostgresBeerStyleRepository;
-  let backup: IBackup;
-  let db: IMemoryDb;
+
   let dataSource: DataSource;
   let repository: Repository<BeerStyleDBEntity>;
 
   beforeAll(async () => {
-    const mockedDbConnection = makemockedDbConnection([BeerStyleDBEntity]);
-
-    db = mockedDbConnection.db;
-    dataSource = mockedDbConnection.dataSource;
+    dataSource = PostgresDataSource;
 
     await dataSource.initialize();
-    await dataSource.synchronize();
-    backup = db.backup();
     repository = dataSource.getRepository(BeerStyleDBEntity);
   });
 
@@ -30,8 +24,11 @@ describe("PostgresBeerStyleRepository tests", () => {
   });
 
   beforeEach(() => {
-    backup.restore();
     sut = new PostgresBeerStyleRepository(dataSource);
+  });
+
+  afterEach(async () => {
+    await repository.clear();
   });
 
   describe("AddBeerStyle Method", () => {
@@ -76,6 +73,19 @@ describe("PostgresBeerStyleRepository tests", () => {
       const beerstyle = await sut.findOne("0cef8e10-7c27-11ed-a1eb-0242ac120002");
 
       expect(beerstyle).toBe(null);
+    });
+  });
+
+  describe("findByTemperatureAverage", () => {
+    test("should return an beerstyle", async () => {
+      const mockedInsert = await repository.save(makeBeerStyleDTO());
+      const { minTemperature, maxTemperature, ...mockedInsertWithouTemperature } = mockedInsert;
+
+      const beerstyle = await sut.findByTemperatureAverage(5);
+
+      expect(beerstyle).toEqual([
+        Object.assign({}, mockedInsertWithouTemperature, { average: "5" })
+      ]);
     });
   });
 
