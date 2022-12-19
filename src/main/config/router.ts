dd76069230a0607485ed4dc631203ee192cc9c07
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-12-19 12:54:40 -0300

    refactor: ajustes

--- src/main/config/router.ts
+++ src/main/config/router.ts
@@ -5,6 +5,11 @@ import { Express, Router } from "express";
 export const setupRoutes = (app: Express): void => {
   const routes = Router();
 
+  routes.use("/status", (req, res) => {
+    res.json({
+      message: "application is running"
+    });
+  });
   routes.use("/api", beerStyleRoutes);
   routes.use("/api", playlistRoutes);
 
--- src/main/index.ts
+++ src/main/index.ts
@@ -5,11 +5,11 @@ import { app } from "@/main/config/app";
 import { PostgresDataSource } from "@/infra/database/pg-datasource";
 import { env } from "@/main/config/env";
 
-const port = env.port ?? "8000";
+const port = env.port ?? "8080";
 
 PostgresDataSource.initialize()
   .then(() => {
-    app.listen(port, () => console.log(`server running at http://localhost:${port}`));
+    app.listen(port, () => console.log(`server running at http://localhost:${port}/status`));
   })
   .catch((e) => {
     console.error(e);
